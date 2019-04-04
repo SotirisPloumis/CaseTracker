3d89b7d52fe885eb850be9acf1904f0cc6893b36
Sotiris Ploumis <sotiris.ploumis@gmail.com>
2019-04-04 22:49:03 +0300

    Add project files.

---
+++ CaseTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CaseTracker.MvcApplication" Language="C#" %>
---
+++ CaseTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CaseTracker.Startup))]
+namespace CaseTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
