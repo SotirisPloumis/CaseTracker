ca3358ab0ef2eb22d89e8716d5e4bd4f158f8123
KostasMihawk <kwsmichopoulos@gmail.com>
2019-04-06 22:11:48 +0300

    createCase Controller and Case Views

---
+++ CaseTracker/Controllers/CaseController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CaseTracker.Models;
+using CaseTracker.Repository;
+
+namespace CaseTracker.Controllers
+{
+    public class CaseController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Case
+        public ActionResult Index()
+        {
+            return View(db.Cases.ToList());
+        }
+
+        // GET: Case/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Case @case = db.Cases.Find(id);
+            if (@case == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@case);
+        }
+
+        // GET: Case/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Case/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Aa,Type")] Case @case)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cases.Add(@case);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(@case);
+        }
+
+        // GET: Case/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Case @case = db.Cases.Find(id);
+            if (@case == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@case);
+        }
+
+        // POST: Case/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Aa,Type")] Case @case)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@case).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(@case);
+        }
+
+        // GET: Case/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Case @case = db.Cases.Find(id);
+            if (@case == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@case);
+        }
+
+        // POST: Case/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Case @case = db.Cases.Find(id);
+            db.Cases.Remove(@case);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
