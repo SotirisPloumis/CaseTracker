e3d54bde26c26cdc5d02777e7f2f9e774dc4c2f4
KostasMihawk <kwsmichopoulos@gmail.com>
2019-04-30 22:24:06 +0300

    fml

--- CaseTracker/Migrations/201904072044307_BigMerge.cs
+++ CaseTracker/Migrations/201904072044307_BigMerge.cs
@@ -3,10 +3,38 @@ namespace CaseTracker.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class initialTest : DbMigration
+    public partial class BigMerge : DbMigration
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.Attorneys",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        AFM = c.String(),
+                        City = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CaseParties",
+                c => new
+                    {
+                        CaseId = c.Int(nullable: false),
+                        PartyId = c.Int(nullable: false),
+                        RoleId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.CaseId, t.PartyId })
+                .ForeignKey("dbo.Cases", t => t.CaseId, cascadeDelete: true)
+                .ForeignKey("dbo.CaseRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.Parties", t => t.PartyId, cascadeDelete: true)
+                .Index(t => t.CaseId)
+                .Index(t => t.PartyId)
+                .Index(t => t.RoleId);
+            
             CreateTable(
                 "dbo.Cases",
                 c => new
@@ -14,6 +42,45 @@ namespace CaseTracker.Migrations
                         Id = c.Int(nullable: false, identity: true),
                         Aa = c.Int(nullable: false),
                         Type = c.String(),
+                        CourtId = c.Int(nullable: false),
+                        AttorneyId = c.Int(nullable: false),
+                        DateOfAssignment = c.DateTime(nullable: false),
+                        DateOfSubmission = c.DateTime(nullable: false),
+                        DateOfEnd = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Attorneys", t => t.AttorneyId, cascadeDelete: true)
+                .ForeignKey("dbo.Courts", t => t.CourtId, cascadeDelete: true)
+                .Index(t => t.CourtId)
+                .Index(t => t.AttorneyId);
+            
+            CreateTable(
+                "dbo.Courts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CaseRoles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Parties",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        AFM = c.String(),
+                        IDCard = c.String(),
                     })
                 .PrimaryKey(t => t.Id);
             
@@ -93,18 +160,33 @@ namespace CaseTracker.Migrations
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.CaseParties", "PartyId", "dbo.Parties");
+            DropForeignKey("dbo.CaseParties", "RoleId", "dbo.CaseRoles");
+            DropForeignKey("dbo.CaseParties", "CaseId", "dbo.Cases");
+            DropForeignKey("dbo.Cases", "CourtId", "dbo.Courts");
+            DropForeignKey("dbo.Cases", "AttorneyId", "dbo.Attorneys");
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
             DropIndex("dbo.AspNetUsers", "UserNameIndex");
             DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
             DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Cases", new[] { "AttorneyId" });
+            DropIndex("dbo.Cases", new[] { "CourtId" });
+            DropIndex("dbo.CaseParties", new[] { "RoleId" });
+            DropIndex("dbo.CaseParties", new[] { "PartyId" });
+            DropIndex("dbo.CaseParties", new[] { "CaseId" });
             DropTable("dbo.AspNetUserLogins");
             DropTable("dbo.AspNetUserClaims");
             DropTable("dbo.AspNetUsers");
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Parties");
+            DropTable("dbo.CaseRoles");
+            DropTable("dbo.Courts");
             DropTable("dbo.Cases");
+            DropTable("dbo.CaseParties");
+            DropTable("dbo.Attorneys");
         }
     }
 }
